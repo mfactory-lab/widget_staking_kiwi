ff139692a2c00bc31cf426a813bacc7d5b0f6580
leukhinroman <leukhinroman@gmail.com>
2022-04-04 19:03:57 +0300

    fix apy chart: avg -> avr

--- src/components/staking/charts/ApyChart.vue
+++ src/components/staking/charts/ApyChart.vue
@@ -95,7 +95,7 @@
 
       const averageData = computed(() => {
         return {
-          name: 'Avg. APY',
+          name: 'Avr. APY',
           type: 'line',
           data: averageApy.value.map((item) => item.apy * 100),
           // data: categories.value.map((item) => {
