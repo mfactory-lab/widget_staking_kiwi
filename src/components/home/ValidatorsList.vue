c0cd4be40e934a6db2de97999b6f2f2e51779df5
leukhinroman <leukhinroman@gmail.com>
2022-04-13 15:54:37 +0300

    more preloaded items

--- src/components/home/ValidatorsList.vue
+++ src/components/home/ValidatorsList.vue
@@ -328,8 +328,8 @@
             :items="itemsShowed"
             virtual-scroll-item-size="123"
             virtual-scroll-slice-size="20"
-            virtual-scroll-slice-ratio-after="0.5"
-            virtual-scroll-slice-ratio-before="0.5"
+            virtual-scroll-slice-ratio-after="0.75"
+            virtual-scroll-slice-ratio-before="1"
           >
             <template #default="{ item, index }">
               <div
