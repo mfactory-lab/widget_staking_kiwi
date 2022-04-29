f444181fb0182743ce9539831087b09d21340fe5
tiamo <vk.tiamo@gmail.com>
2022-04-29 04:08:23 +0300

    fix: api url

--- src/config/common.ts
+++ src/config/common.ts
@@ -40,9 +40,11 @@ export const POOL_RELOAD_INTERVAL = 30000;
 export const DEFAULT_APY = 0.07;
 export const APY_VALIDATOR_ID = null;
 
-// misc
-export const API_URL = 'https://api.thevalidators.io';
+// api
+export const API_URL = import.meta.env.VITE_API_URL;
+export const API_COLLECTOR_URL = 'https://api.thevalidators.io';
 
+// misc
 export const JSOL_LOGO =
   'https://raw.githubusercontent.com/mfactory-lab/jpool-pub/main/assets/images/jsol.png';
 
