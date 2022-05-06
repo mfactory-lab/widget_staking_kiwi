b3c5186b4afe3d242e4f5a58874a6ad2e3bc4b2c
MGrgr <grkapran@gmail.com>
2022-05-06 15:30:50 +0300

    fix: config

--- src/config/validator.ts
+++ src/config/validator.ts
@@ -1,10 +1,3 @@
-import ethIcon from '@/assets/img/eth.svg';
-import dotIcon from '@/assets/img/dot.svg';
-import atomIcon from '@/assets/img/atom.svg';
-import terraIcon from '@/assets/img/terra.svg';
-import avalancheIcon from '@/assets/img/avalanche.svg';
-import minaIcon from '@/assets/img/mina.svg';
-
 export const DEFAULT_VALIDATOR = {
   'mainnet-beta': {
     idPubkey: '8yjHdsCgx3bp2zEwGiWSMgwpFaCSzfYAHT1vk7KJBqhN',
@@ -16,36 +9,3 @@ export const DEFAULT_VALIDATOR = {
     voterKey: 'BcX6qjy6fxYSHPyRmy5uJV6Z9MzK8v5ZS8UUiREoEYWW',
   },
 };
-
-export const LAUNCH_VALIDATORS = [
-  {
-    title: 'Etherium 2.0',
-    id: 'ethereum',
-    icon: ethIcon,
-  },
-  {
-    title: 'Polkadot',
-    id: 'polkadot',
-    icon: dotIcon,
-  },
-  {
-    title: 'Cosmos',
-    id: 'cosmos',
-    icon: atomIcon,
-  },
-  {
-    title: 'Terra',
-    id: 'terracoin',
-    icon: terraIcon,
-  },
-  {
-    title: 'Avalanche',
-    id: 'avalanche-2',
-    icon: avalancheIcon,
-  },
-  {
-    title: 'Mina',
-    id: 'mina-protocol',
-    icon: minaIcon,
-  },
-];
